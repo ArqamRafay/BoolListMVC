771c743a2a4b294b578d43c640ec38ae274dd2e7
ArqamRafay <getarqamnow@gmail.com>
2021-09-25 20:11:49 +0500

    complete course bit bugs arrive

---
+++ BoolListMVC/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using BoolListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoolListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //API Call 
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { date = await _db.Books.ToListAsync() });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var _bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (_bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Books.Remove(_bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+    }
+}
--- BoolListMVC/Startup.cs
+++ BoolListMVC/Startup.cs
@@ -1,9 +1,12 @@
+using BoolListMVC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +26,7 @@ namespace BoolListMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
